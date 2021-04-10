c636b88097a2d8fd980649b38898bb0989453b3b
NinaLukashenko <nina.lucashenco@gmail.com>
2021-04-10 20:38:37 +0300

    Filters are done!

--- src/main.js
+++ src/main.js
@@ -5,42 +5,45 @@ import { createFilmCardTemplate } from './view/film-card.js';
 import { createShowMoreButtonTemplate } from './view/show-more-button.js';
 import { createFilmDetailsPopupTemplate } from './view/film-details-popup.js';
 import { generateFilm } from './mock/film.js';
+import { createFooterStatisticsTemplate } from './view/footer-statistics.js';
+import { generateFilter } from './mock/filter.js';
 
-const FILMS_QUANTITY = 1;
+const FILMS_QUANTITY = 8;
 const FILMS_STEP = 5;
 
 const films = new Array(FILMS_QUANTITY).fill().map(generateFilm);
+const filters = generateFilter(films);
 console.log(films);
 
 // Количество фильмов для отрисовки
 let renderedFilmsQuantity = FILMS_QUANTITY >= FILMS_STEP ? FILMS_STEP : FILMS_QUANTITY;
 
-const render = (container, template, place) => {
+const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
 
 // Звание пользователя
 const headerElement = document.querySelector('.header');
-render(headerElement, createUserRankTemplate(), 'beforeend');
+render(headerElement, createUserRankTemplate());
 
 // Меню
 const siteMainElement = document.querySelector('.main');
-render(siteMainElement, createSiteMenuTemplate(), 'beforeend');
+render(siteMainElement, createSiteMenuTemplate(filters));
 
 // Контент
-render(siteMainElement, createFilmsTemplate(), 'beforeend');
+render(siteMainElement, createFilmsTemplate());
 
 const filmsListContainerElement = siteMainElement.querySelector('.films .films-list .films-list__container');
 
 for (let i = 0; i < renderedFilmsQuantity; i++) {
-  render(filmsListContainerElement, createFilmCardTemplate(films[i]), 'beforeend');
+  render(filmsListContainerElement, createFilmCardTemplate(films[i]));
 }
 
 // Кнопка "Show more"
 const filmsListElement = siteMainElement.querySelector('.films-list');
 
 if (FILMS_QUANTITY > renderedFilmsQuantity) {
-  render(filmsListElement, createShowMoreButtonTemplate(), 'beforeend');
+  render(filmsListElement, createShowMoreButtonTemplate());
 
   const showMoreElement = filmsListElement.querySelector('.films-list__show-more');
 
@@ -49,7 +52,7 @@ if (FILMS_QUANTITY > renderedFilmsQuantity) {
     films
       .slice(renderedFilmsQuantity, renderedFilmsQuantity + FILMS_STEP)
       .forEach((item) => {
-        render(filmsListContainerElement, createFilmCardTemplate(item), 'beforeend');
+        render(filmsListContainerElement, createFilmCardTemplate(item));
       });
 
     renderedFilmsQuantity += FILMS_STEP;
@@ -62,6 +65,11 @@ if (FILMS_QUANTITY > renderedFilmsQuantity) {
 
 }
 
-// Попап
-const footerElement = document.querySelector('.footer');
-render(footerElement, createFilmDetailsPopupTemplate(films[0]), 'afterend');
+// Статистика в подвале
+const footerStatisticsElement = document.querySelector('.footer__statistics');
+render(footerStatisticsElement, createFooterStatisticsTemplate(films.length));
+
+
+// // Попап
+// const footerElement = document.querySelector('.footer');
+// render(footerElement, createFilmDetailsPopupTemplate(films[0]), 'afterend');
--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,10 +1,27 @@
-export const createSiteMenuTemplate = () => {
+const createFilterItemTemplate = (filter, isActive) => {
+  const {name, count} = filter;
+
+  const filterClassName = isActive
+    ? 'main-navigation__item main-navigation__item--active'
+    : 'main-navigation__item';
+
+  const filterCountElement = isActive
+    ? ''
+    : `<span class="main-navigation__item-count">${count}</span>`;
+
+  return `
+      <a href="#${name.toLowerCase()}" class="${filterClassName}">${name} ${filterCountElement}</a>
+    `;
+};
+
+export const createSiteMenuTemplate = (filters) => {
+  const filterItemsTemplate = filters
+    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
+    .join('');
+
   return `<nav class="main-navigation">
     <div class="main-navigation__items">
-      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+      ${filterItemsTemplate}
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>`;
