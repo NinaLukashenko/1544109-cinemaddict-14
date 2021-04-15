75f6e12e4b1802dd2ae3cc4f9dc002d2348287fc
NinaLukashenko <nina.lucashenco@gmail.com>
2021-04-15 11:15:38 +0300

    UserRank is switched to class!

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { createUserRankTemplate } from './view/user-rank.js';
 import { createSiteMenuTemplate } from './view/site-menu.js';
 import { createFilmsTemplate } from './view/films.js';
 import { createFilmCardTemplate } from './view/film-card.js';
@@ -8,9 +7,9 @@ import { generateFilm } from './mock/film.js';
 import { createFooterStatisticsTemplate } from './view/footer-statistics.js';
 import { generateFilter } from './mock/filter.js';
 import { createSortTemplate } from './view/sort.js';
-
-const FILMS_QUANTITY = 13;
-const FILMS_STEP = 5;
+import { FILMS_QUANTITY, FILMS_STEP } from './const.js';
+import { render, createElement, RenderPosition } from './utils.js';
+import UserRankView from './view/user-rank.js'; // импорт по умолчанию (фигурные скобки не нужны)
 
 const films = new Array(FILMS_QUANTITY).fill().map(generateFilm);
 const filters = generateFilter(films);
@@ -18,60 +17,57 @@ const filters = generateFilter(films);
 // Количество фильмов для отрисовки
 let renderedFilmsQuantity = FILMS_QUANTITY >= FILMS_STEP ? FILMS_STEP : FILMS_QUANTITY;
 
-const render = (container, template, place = 'beforeend') => {
-  container.insertAdjacentHTML(place, template);
-};
 
 // Звание пользователя
 const headerElement = document.querySelector('.header');
-render(headerElement, createUserRankTemplate());
+render(headerElement, new UserRankView().getElement(), RenderPosition.BEFOREEND );
 
-// Меню
-const siteMainElement = document.querySelector('.main');
-render(siteMainElement, createSiteMenuTemplate(filters));
+// // Меню
+// const siteMainElement = document.querySelector('.main');
+// render(siteMainElement, createSiteMenuTemplate(filters));
 
-render(siteMainElement, createSortTemplate());
+// render(siteMainElement, createSortTemplate());
 
-// Контент
-render(siteMainElement, createFilmsTemplate());
+// // Контент
+// render(siteMainElement, createFilmsTemplate());
 
-const filmsListContainerElement = siteMainElement.querySelector('.films .films-list .films-list__container');
+// const filmsListContainerElement = siteMainElement.querySelector('.films .films-list .films-list__container');
 
-for (let i = 0; i < renderedFilmsQuantity; i++) {
-  render(filmsListContainerElement, createFilmCardTemplate(films[i]));
-}
+// for (let i = 0; i < renderedFilmsQuantity; i++) {
+//   render(filmsListContainerElement, createFilmCardTemplate(films[i]));
+// }
 
-// Кнопка "Show more"
-const filmsListElement = siteMainElement.querySelector('.films-list');
+// // Кнопка "Show more"
+// const filmsListElement = siteMainElement.querySelector('.films-list');
 
-if (FILMS_QUANTITY > renderedFilmsQuantity) {
-  render(filmsListElement, createShowMoreButtonTemplate());
+// if (FILMS_QUANTITY > renderedFilmsQuantity) {
+//   render(filmsListElement, createShowMoreButtonTemplate());
 
-  const showMoreElement = filmsListElement.querySelector('.films-list__show-more');
+//   const showMoreElement = filmsListElement.querySelector('.films-list__show-more');
 
-  showMoreElement.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    films
-      .slice(renderedFilmsQuantity, renderedFilmsQuantity + FILMS_STEP)
-      .forEach((item) => {
-        render(filmsListContainerElement, createFilmCardTemplate(item));
-      });
+//   showMoreElement.addEventListener('click', (evt) => {
+//     evt.preventDefault();
+//     films
+//       .slice(renderedFilmsQuantity, renderedFilmsQuantity + FILMS_STEP)
+//       .forEach((item) => {
+//         render(filmsListContainerElement, createFilmCardTemplate(item));
+//       });
 
-    renderedFilmsQuantity += FILMS_STEP;
+//     renderedFilmsQuantity += FILMS_STEP;
 
-    // Проверка нужно ли прятать кнопку
-    if (FILMS_QUANTITY <= renderedFilmsQuantity) {
-      showMoreElement.remove();
-    }
-  });
+//     // Проверка нужно ли прятать кнопку
+//     if (FILMS_QUANTITY <= renderedFilmsQuantity) {
+//       showMoreElement.remove();
+//     }
+//   });
 
-}
+// }
 
-// Статистика в подвале
-const footerStatisticsElement = document.querySelector('.footer__statistics');
-render(footerStatisticsElement, createFooterStatisticsTemplate(films.length));
+// // Статистика в подвале
+// const footerStatisticsElement = document.querySelector('.footer__statistics');
+// render(footerStatisticsElement, createFooterStatisticsTemplate(films.length));
 
 
-// Попап
-const footerElement = document.querySelector('.footer');
-render(footerElement, createFilmDetailsPopupTemplate(films[0]), 'afterend');
+// // Попап
+// const footerElement = document.querySelector('.footer');
+// render(footerElement, createFilmDetailsPopupTemplate(films[0]), 'afterend');
